e8139da742f9c4f9dee4ed380178aaa3d14ab9fc
ahamed-wafiq <wafiqahamed51@gmail.com>
2025-03-21 22:18:50 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,96 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".nav-bar", {
+    duration: 1.5,
+    y: -100,
+    opacity: 0,
+    ease: "bounce"
+});
+
+gsap.from(".nav", {
+    duration: 1,
+    opacity: 0,
+    y: -20,
+    stagger: 0.2,
+});
+const logo=document.querySelectorAll("#logo path");
+for(let i=0; i<logo.length;i++){
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
+
+gsap.to(".planet-container", {
+    rotationY: 360,
+    rotationX:360,
+    duration: 8,
+    repeat: -1,
+    ease: "linear"
+});
+
+gsap.from(".rec",{
+    x:2000,
+    duration:5,
+})
+gsap.from(".card", {
+    duration: 1.2,
+    y: 50,
+    opacity: 0,
+    stagger: 0.2,
+    ease: "power3.out"
+});
+
+
+gsap.from("footer", {
+    duration: 1,
+    opacity: 0,
+    y: 20,
+    ease: "power1.out"
+});
+
+gsap.utils.toArray(".section1").forEach((section, i) => {
+    const colors = ["#1a1a2e", "#16213e", "#0f3460", "#53354a"];
+    gsap.to(section, {
+        backgroundColor: colors[i % colors.length],
+        scrollTrigger: {
+            trigger: section,
+            start: "top center",
+            end: "bottom center",
+            scrub: true
+        }
+    });
+});
+gsap.utils.toArray(".section2").forEach((section, i) => {
+    const colors = ["#1a1a2e", "#16213e", "#0f3460", "#53354a"];
+    gsap.to(section, {
+        backgroundColor: colors[i % colors.length],
+        scrollTrigger: {
+            trigger: section,
+            start: "top center",
+            end: "bottom center",
+            scrub: true
+        }
+    });
+});
+gsap.utils.toArray(".section3").forEach((section, i) => {
+    const colors = ["#1a1a2e", "#16213e", "#0f3460", "#53354a"];
+    gsap.to(section, {
+        backgroundColor: colors[i % colors.length],
+        scrollTrigger: {
+            trigger: section,
+            start: "top center",
+            end: "bottom center",
+            scrub: true
+        }
+    });
+});
+gsap.utils.toArray(".contact").forEach((section, i) => {
+    const colors = ["#1a1a2e", "#16213e", "#0f3460", "#53354a"];
+    gsap.to(section, {
+        backgroundColor: colors[i % colors.length],
+        scrollTrigger: {
+            trigger: section,
+            start: "top center",
+            end: "bottom center",
+            scrub: true
+        }
+    });
+});
